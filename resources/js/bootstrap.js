e9b66c9ec9eedf8d9d897943d03fcc5152c014ef
Hendri Juhanda <hendri.juhanda@gmail.com>
2024-03-19 22:04:37 +0700

    feat: configured vite vue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
-import axios from 'axios';
-window.axios = axios;
+import("./bootstrap");
+import { createApp } from "vue";
+import App from "../src/App.vue";
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+createApp(App).mount("#app");
