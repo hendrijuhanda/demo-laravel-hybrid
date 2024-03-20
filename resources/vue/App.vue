0256cc1e623eae0b15c38f43cc0c84c8a004fc57
Hendri Juhanda <hendri.juhanda@gmail.com>
2024-03-20 23:14:21 +0700

    fix: updated tscondig

--- resources/vue/App.vue
+++ resources/vue/App.vue
@@ -5,23 +5,21 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <ConfigProvider
-    :theme="{
-      token: {
+<ConfigProvider :theme="{
+    token: {
         colorPrimary: '#d31b8e',
-      },
-    }"
-  >
+    },
+}">
     <StyleProvider hashPriority="high">
-      <template v-if="$route.meta.layout === 'login'">
-        <RouterView />
-      </template>
+        <template v-if="$route.meta.layout === 'login'">
+            <RouterView />
+        </template>
 
-      <template v-else-if="$route.meta.layout === 'main'">
-        <Main>
-          <RouterView />
-        </Main>
-      </template>
+        <template v-else-if="$route.meta.layout === 'main'">
+            <Main>
+                <RouterView />
+            </Main>
+        </template>
     </StyleProvider>
-  </ConfigProvider>
+</ConfigProvider>
 </template>
